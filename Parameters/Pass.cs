3183343b8f17860887e0083f10d2196eb440299f
Alexander Gough <gougha3@my.erau.edu>
2018-08-31 12:01:51 -0400

    Lab 4b

---
+++ Parameters/Pass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Parameters
+{
+    class Pass
+    {
+        public static void Refrence(WrappedInt param)
+        {
+            param.Number = 42;
+        }
+        public static void Value(int param)
+        {
+            param = 42;
+        }
+    }
+}
